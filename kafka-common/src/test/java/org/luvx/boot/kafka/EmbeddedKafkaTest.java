ea41c7773405fd31e442d951b4850c8c24bfe1e4
F.LuVx <yeyu0926@hotmail.com>
2023-03-22 11:07:57 +0800

    内嵌 kafka

---
+++ kafka-common/src/test/java/org/luvx/boot/kafka/EmbeddedKafkaTest.java
@@ -0,0 +1,32 @@
+package org.luvx.boot.kafka;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.springframework.kafka.test.EmbeddedKafkaBroker.BROKER_LIST_PROPERTY;
+
+@ExtendWith(SpringExtension.class)
+@EmbeddedKafka(
+        count = 4,
+        ports = {59092, 59093, 59094, 59095},
+        partitions = 3,
+        zookeeperPort = 52181,
+        topics = {"embedded-spring"}
+)
+@ImportAutoConfiguration(KafkaAutoConfiguration.class)
+public class EmbeddedKafkaTest {
+    static {
+        System.setProperty(BROKER_LIST_PROPERTY, "spring.kafka.bootstrap-servers");
+    }
+
+    @Test
+    public void run() throws IOException {
+        System.in.read();
+    }
+}
